a7c1c76676e4039e1248f0f21bef302df2fd22a3
Nitin <nitinmlvya@gmail.com>
2015-02-06 01:56:19 +0530

    Added Images but

---
+++ assets/images/9 Crazy jQuery Chat and Chat Box Plugins_files/container_data/iframe_data/fetch.js
@@ -0,0 +1,5 @@
+var externCrIds7011 = {};
+var scriptTags7011= [];
+var nonScriptTags7011= [];
+nonScriptTags7011.push({'isVisible': 'false' , 'tag': '<img src="http://godaddy.sp1.convertro.com/view/vt/v1/godaddy/18/cvo.gif?cvosrc=ret.@@SIID@@.@@CRID@@&cvo_campaign=&cvo_crid=@@CRID@@&cvo_pid=@@PLID@@" width=1 height=1/>', 'secureTag': '<img src="https://godaddy.sp1.convertro.com/view/vt/v1/godaddy/18/cvo.gif?cvosrc=ret.@@SIID@@.@@CRID@@&cvo_campaign=&cvo_crid=@@CRID@@&cvo_pid=@@PLID@@" width=1 height=1/>' });
+te_tags.AddTags(scriptTags7011, nonScriptTags7011, externCrIds7011);
---
+++ assets/images/9 Crazy jQuery Chat and Chat Box Plugins_files/container_data_002/iframe_data/fetch.js
@@ -0,0 +1,5 @@
+var externCrIds2343 = {};
+var scriptTags2343= [];
+var nonScriptTags2343= [];
+nonScriptTags2343.push({'isVisible': 'false' , 'tag': '<img src="http://godaddy.sp1.convertro.com/view/vt/v1/godaddy/18/cvo.gif?cvosrc=ret.@@SIID@@.@@CRID@@&cvo_campaign=&cvo_crid=@@CRID@@&cvo_pid=@@PLID@@" width=1 height=1/>', 'secureTag': '<img src="https://godaddy.sp1.convertro.com/view/vt/v1/godaddy/18/cvo.gif?cvosrc=ret.@@SIID@@.@@CRID@@&cvo_campaign=&cvo_crid=@@CRID@@&cvo_pid=@@PLID@@" width=1 height=1/>' });
+te_tags.AddTags(scriptTags2343, nonScriptTags2343, externCrIds2343);
