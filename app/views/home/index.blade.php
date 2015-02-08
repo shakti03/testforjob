4897a1bfc58c847c2b2c05f2a7653c6c4ffc91f5
Nitin <nitinmlvya@gmail.com>
2015-02-08 23:08:07 +0530

    Added index.blade.php to view/home

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,63 +1,113 @@
 <html>
     <head>
         <title> Test For Job </title>
-        <!-- css -->
-        {{ HTML::style('assets/css/bootstrap.min.css') }}
-        {{ HTML::style('assets/css/bootstrap-theme.min.css') }}
-        {{ HTML::style('assets/css/strength.css') }}
-        {{ HTML::style('assets/css/style.css') }}
-        {{ HTML::style('assets/css/color-font.css') }}
-        {{ HTML::style('assets/css/cindex.css')}}
-        
-        <!-- js -->
-        {{ HTML::script('assets/js/jquery-1.10.2.min.js') }}
-        {{ HTML::script('assets/js/vendor/password_strength/strength.js') }}
-        {{ HTML::script('assets/js/bootstrap.min.js') }}
-        {{ HTML::script('assets/js/live_chat.js') }}
-        {{ HTML::script('assets/js/home/custom.js')}}
-
-        <script type="text/javascript">
-            base_url = "{{ URL::to('/')}}";
-        </script> 
-        
-
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+            <link href="css/cindex.css" rel="stylesheet">
+            <script src="js/jquery.1.9.1.js" type="text/javascript"></script>
+            <script src="js/live_chat.js" type="text/javascript"></script>
+            <script src="js/carousel.js"></script>
     </head>
     <body>
-        @if(Session::has('reminder-success'))
-            afafafaaaaaaaaaa
-        @endif
-        <div class="row header" style="z-index:2;">
-            <div class="col-md-2">
-                <img style="height: 50px;" src="{{URL::to('/')}}/assets/images/1.jpg"/>
+        <div class="row header">
+            <div class="col-md-3">
+                <img width="50px;" src="images/logo.png"/>
             </div>
-            <div class="col-md-3">&nbsp;
-            </div>
-            <div class="col-md-5">
+            <div class="col-md-7">
                 <ul>
-                    <li>What is SmarT?</li>
-                    <li>FAQ</li>
-                    <li>User Stories</li>
-                    <li>Plans</li>
-                    <li>Schools</li>
+                    <li>IT-Freshers</li>
+                    <li>IT-Experience</li>
+                    <li>IT-Technologies</li>
+                    <li>Test Series</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
                 </ul>
             </div>
             <div class="col-md-2">
-                <a id="btnlogin" class="btn btn-default" data-target="#login" data-toggle="modal"  href="javascript:void(0);">Login</a>
-
-                <a id="btnSignup" style="margin-left: -5px;" class="btn borderNone btn-warning" data-target="#signup" data-toggle="modal"  href="javascript:void(0);">SignUp</a>
+                <a class="btn btn-default" href="#">Login</a>
+                <a style="margin-left: -5px;" class="btn btn-warning" href="#">SignUp</a>
             </div>
         </div>
         
-        <!-- Slider -->
-        <div class="row slider">
-            <div class="col-md-12">
-                <img class="bg-img" src="{{URL::to('/')}}/assets/images/temp_feature1.png"/>
+        <div class="row ">
+            <div class="col-md-12 below-head">
+                <h3 class="text-center">TEST FOR JOB</h3>
+                <!--<img width="100%" src="images/temp_feature1.png"/>-->
             </div>
-            <div class="col-md-12">
-                <div class="fg-img"></div>
-            </div>
-            
         </div>
+        <!--<div class="row">-->
+        <!--    <div class="col-md-12 slider">-->
+        <!--        <img width="105%" src="images/a1.jpg"/>-->
+        <!--    </div>-->
+        <!--    <!--<div class="col-md-12">-->
+        <!--        <div class="fg-img">Nitin Solanki</div>-->
+        <!--    </div>-->
+        <!--</div>-->
+
+<!-- Slider -->
+<div class="row-fluid">
+    <div id="carousel-example-generic" class="col-md-12 carousel slider" data-ride="carousel">
+     
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+      </ol>
+
+      <!-- Slider Content (Wrapper for slides )-->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img width="100%" src="images/a1.jpg">
+            <!--<div class="carousel-caption">-->
+            <!--</div>-->
+        </div>
+        <div class="item">
+          <img width="100%" src="images/a2.jpg">
+            <!--<div class="carousel-caption">-->
+            <!--</div>-->
+        </div>
+        <div class="item">
+          <img width="100%" src="images/a3.jpg">
+            <!--<div class="carousel-caption">-->
+            <!--</div>-->
+        </div>
+        <div class="item">
+          <img width="100%" src="images/a4.jpg">
+            <!--<div class="carousel-caption">-->
+            <!--</div>-->
+        </div>
+        <div class="item">
+          <img width="100%" src="images/a5.jpg">
+            <!--<div class="carousel-caption">-->
+            <!--</div>-->
+        </div>
+      </div>
+     
+      <!-- Controls -->
+      
+      <a style="height:0%" class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span style="margin-top:50%;"  class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a style="height:0%" class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span style="margin-top:50%;" class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>
+</div>
+        
+        
+        
+        
+        <!--<div class="row">
+            <div class="col-md-12">
+                <img class="" src="images/temp_feature1.png"/>
+            </div>-->
+            <!--<div class="col-md-12">
+                <div class="fg-img">Nitin Solanki</div>
+            </div>-->
+
+        <!--</div>-->
         
         <!-- Features Info -->
         <div class="feature">
@@ -70,86 +120,86 @@
                 </div>
                 <div class="col-md-12">
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_1.jpg"/>
+                        <img class="img-responsive" src="images/feature_1.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_2.jpg"/>
+                        <img class="img-responsive" src="images/feature_2.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_3.jpg"/>
+                        <img class="img-responsive" src="images/feature_3.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_4.jpg"/>
+                        <img class="img-responsive" src="images/feature_4.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_5.jpg"/>
+                        <img class="img-responsive" src="images/feature_5.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_6.jpg"/>
+                        <img class="img-responsive" src="images/feature_6.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_7.jpg"/>
+                        <img class="img-responsive" src="images/feature_7.jpg"/>
                     </div>
                     <div class="col-md-3">
-                        <img class="img-responsive" src="{{URL::to('/')}}/assets/images/feature_8.jpg"/>
+                        <img class="img-responsive" src="images/feature_8.jpg"/>
                     </div>
                 </div>
             </div>
         </div>
         
         <!-- Second Features Info -->
-        <div class="feature">
+        <div class="feature" style="margin-top:0%;">
             <div class="container">
                 <div class="col-md-12">
                     <h1 class="col-md-9">Features</h1>
-                    <a class="col-md-3"><img style="margin-top: 15%;" src="{{URL::to('/')}}/assets/images/know_more.jpg"/></a>
+                    <a class="col-md-3"><img style="margin-top: 15%;" src="images/know_more.jpg"/></a>
                 </div>
                 <div class="col-md-12">
                     <h5>Get fully features TCY Analytics which will help you prepare, compare and share your performance with your friends.</h5>
                 </div>
                 <div class="col-md-12">
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_1.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_1.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_2.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_2.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_3.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_3.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_4.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_4.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_5.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_5.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_6.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_6.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_7.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_7.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_8.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_8.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
                     <div class="col-md-4">
-                        <img width="50px" height="50px" class="img-circle" src="{{URL::to('/')}}/assets/images/icon_feature_9.jpg"/>
+                        <img width="50px" height="50px" class="img-circle" src="images/icon_feature_9.jpg"/>
                         <h4>High Quality Content</h4>
                         <p class="lead">Videos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & MockVideos, TopiceWise, Sectional & Mock</p>
                     </div>
@@ -231,36 +281,32 @@
                 </div>
                 <div class="col-md-3">
                     <ul>
-                        <li><a><img src="{{URL::to('/')}}/assets/images/icon_fb.jpg"/></a></li>
-                        <li><a><img src="{{URL::to('/')}}/assets/images/icon_twitter.jpg"/></a></li>
-                        <li><a><img src="{{URL::to('/')}}/assets/images/icon_google_plus.jpg"/></a></li>
+                        <li><a><img src="images/icon_fb.jpg"/></a></li>
+                        <li><a><img src="images/icon_twitter.jpg"/></a></li>
+                        <li><a><img src="images/icon_google_plus.jpg"/></a></li>
                     </ul>
                 </div>
                 <div class="col-md-4">
                     <ul>
-                        <li><a><img width="80px;" src="{{URL::to('/')}}/assets/images/icon_android.jpg"/></a></li>
-                        <li><a><img width="100px;" src="{{URL::to('/')}}/assets/images/icon_flipkart.jpg"/></a></li>
+                        <li><a><img width="80px;" src="images/icon_android.jpg"/></a></li>
+                        <li><a><img width="100px;" src="images/icon_flipkart.jpg"/></a></li>
                     </ul>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-10"></div>
-                <div class="col-md-2"><a class="live-chat-btn" href="#"><img width="40px;" src="{{URL::to('/')}}/assets/images/icon_chat.png"/>Live chat</a></div>                
+                <div class="col-md-2"><a class="live-chat-btn" href="#"><img width="40px;" src="images/icon_chat.png"/>Live chat</a></div>                
             </div>
             <div class="row">
-                <div class="col-md-3 col-md-offset-9 ">                    
-                    <div class="live-chat-window" id='chat_div'></div>
+                <div class="col-md-3 col-md-offset-9 live-chat-window">
                 </div>
                 <div class="col-md-3 col-md-offset-9 live-chat">
-                    <div>Live Chat<a><img class="img-circle float close-initial-chat" width="25px;" src="{{URL::to('/')}}/assets/images/icon_close.png"/></a></div>
+                    <div>Live Chat<a><img class="float close-initial-chat" width="25px;" src="images/icon_close.png"/></a></div>
                     <input class="form-control" name="chat_uname" id="chat_uname" type="text" placeholder="Name"/><br>
                     <!--<input name="chat_uname" id="chat_uname" type="text"/>-->
                     <input class="btn btn-info" name="chat_submit" id="chat_submit" type="button" value="Start Chat"/>
                 </div>
             </div>
         </div>
-        @include('home.partials.login')
-        @include('home.partials.signup')
-        @include('home.partials.forget-password')
     </body>
 </html>
